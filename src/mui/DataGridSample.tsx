b526fcb1ae456abac69b5cab8e8b5c6749dad9e9
yakintech <cagatayyildiz87@gmail.com>
2023-09-15 12:14:12 +0300

    datagrid

--- src/mui/DataGridSample.tsx
+++ src/mui/DataGridSample.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 import { DataGrid, nlNL, trTR } from '@mui/x-data-grid';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
@@ -7,7 +7,22 @@ import { useNavigate, useNavigation } from 'react-router-dom';
 function DataGridSample() {
 
     const [orders, setorders] = useState([]);
+    const [open, setOpen] = React.useState(false);
+    const [id, setid] = useState(0)
 
+
+    const deleteOk = () => {
+        setOpen(false);
+
+        axios.delete('https://northwind.vercel.app/api/orders/' + id)
+            .then(res => {
+                loadData();
+            })
+    };
+
+    const deleteCancel = () => {
+        setOpen(false);
+    };
 
     const navigate = useNavigate();
 
@@ -28,11 +43,13 @@ function DataGridSample() {
 
     const removeOrder = (item: any) => {
 
-        axios.delete('https://northwind.vercel.app/api/orders/' + item.id)
-            .then(res => {
-                loadData();
-            })
+        setOpen(true)
+        setid(item.id)
+
     }
+
+
+    const total = Math.random() * 9999
 
 
     const goDetail = (item: any) => {
@@ -72,7 +89,13 @@ function DataGridSample() {
             headerName: 'Delete',
             width: 200,
             renderCell: (item: any) => <Button onClick={() => removeOrder(item)} color='error' variant='contained'>Delete</Button>
-        }
+        },
+        // {
+        //     field: 'total',
+        //     headerName: 'Total',
+        //     width: 200,
+        //     renderCell: (item: any) => console.log(item)
+        // }
     ]
 
     return (<>
@@ -87,6 +110,28 @@ function DataGridSample() {
                 },
             }}
         />
+
+
+        <Dialog
+            open={open}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">
+                Silme İşlemi
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    Silmek istediğinize Emin misiniz?
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={deleteCancel}>İptal</Button>
+                <Button onClick={deleteOk} autoFocus>
+                    Evet
+                </Button>
+            </DialogActions>
+        </Dialog>
 
     </>
     )
